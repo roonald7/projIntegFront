94435198df54093d15fe66a8db754e795552e32f
Ronald <roonald.anderson017@gmail.com>
2021-01-04 13:17:16 -0300

    Git Ignore

---
+++ src/main/java/App.java
@@ -0,0 +1,46 @@
+package main.java;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import main.java.view.MainScreenController;
+
+public class App extends Application {
+
+	private Stage primaryStage;
+	private AnchorPane rootLayout;
+	
+	@Override
+	public void start(Stage primaryStage) {
+		this.primaryStage = primaryStage;
+		this.primaryStage.setTitle("PROJECT 3");
+		
+		loadMainScreen();
+	}
+	
+	public void loadMainScreen() {
+		try {
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(App.class.getResource("view/MainScreen.fxml"));
+			rootLayout = (AnchorPane) loader.load();
+			
+			// Show the scene containing the root layout.
+			Scene scene = new Scene(rootLayout);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+			
+			MainScreenController controller = loader.getController();
+			controller.setMain(this);
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
---
+++ src/main/java/view/MainScreenController.java
@@ -0,0 +1,106 @@
+package main.java.view;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+import javafx.collections.FXCollections;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TitledPane;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeView;
+import main.java.App;
+import main.java.entity.Medidor;
+import main.java.service.MedidorService;
+
+public class MainScreenController {
+	@FXML
+    private TitledPane paneLinha;
+	
+	@FXML
+	private ComboBox<String> comboBoxLinha;
+
+	@FXML
+	private TitledPane paneMedidor;
+	
+	@FXML
+	private TreeView<String> treeViewModelo;
+	
+	public List<String> linhaData = new ArrayList<String>();
+	
+	public int nLinha = 0;
+	public int nCategoria = 0;
+	
+	private List<Medidor> medidorData;
+	
+	private App mainApp;
+	
+	private MedidorService medidorService = new MedidorService(); 
+	 
+	public void setMain(App mainApp) {
+		this.mainApp = mainApp;
+	}
+	
+	@FXML
+    void lineOptionSelect(ActionEvent event) {
+		String optSel = comboBoxLinha.getSelectionModel().getSelectedItem().toString();
+		loadTreeView(optSel);
+    }
+	
+	@FXML
+	public void initialize() {
+		startScreen();
+	}
+		
+	 
+	public void loadScreen() {
+		paneMedidor.setDisable(true);	
+		
+		medidorData = medidorService.findAll();
+		
+		for(Medidor medidorA : medidorData) {
+			if(!linhaData.contains(medidorA.getLinha())) {			
+				linhaData.add(medidorA.getLinha());
+				nLinha++;
+			}
+		}		 
+		comboBoxLinha.setItems(FXCollections.observableArrayList(linhaData));	
+	}
+	 
+	public void startScreen() {
+		loadScreen();
+	}
+	
+	public void loadTreeView(String optModelo) {
+		paneMedidor.setDisable(false);
+		
+		nCategoria = 0;
+		
+		TreeItem<String> treeViewRoot = new TreeItem<String>(optModelo);
+		treeViewModelo.setRoot(treeViewRoot);
+		
+		List<String> categoriaData = new ArrayList<String>();		
+				
+		for(Medidor medidorA : medidorData) {
+			if(medidorA.getLinha().equals(optModelo)) {
+				
+				if(!categoriaData.contains(medidorA.getCategoria())) {
+					categoriaData.add(medidorA.getCategoria());
+					treeViewRoot.getChildren().add(new TreeItem<String>(medidorA.getCategoria()));
+					treeViewRoot.getChildren().get(nCategoria).getChildren().add(new TreeItem<String>(medidorA.getLinha() + " " + medidorA.getModelo()));
+					nCategoria++;
+				} else {
+					for(int i = 0; i < nCategoria; i++) {
+						if(medidorA.getCategoria().equals(categoriaData.get(i))) {
+							treeViewRoot.getChildren().get(i).getChildren().add(new 
+									TreeItem<String>(medidorA.getLinha() + " " + medidorA.getModelo()));
+						}
+					}
+				}
+			}
+		}
+		
+	}
+}
